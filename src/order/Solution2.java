04ccf4b22bf1a36e568bc79ab61e653aec1d3c1f
fehifh <qisu26@qq.com>
2021-10-30 16:25:33 -0400

    2

---
+++ src/order/Solution2.java
@@ -0,0 +1,16 @@
+package order;
+
+public class Solution2 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int[] sumAndCarry=new int[]{0,0};
+        if(l1.val+l2.val>9){
+
+        }
+        ListNode sum = new ListNode(0);
+
+    }
+
+    private void addTwoDigits(int[] sc, int addent1, int addent2){
+
+    }
+}
